398e972e4e35c14f5b5b1693cbf1cbe4dcc424e5
Pei <jzha471@aucklanduni.ac.nz>
2017-08-13 20:29:48 +1200

    69. Using local reference in templates

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -6,17 +6,19 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
   styleUrls: ['./cockpit.component.css']
 })
 export class CockpitComponent implements OnInit {
-  newServerName = '';
+
   newServerContent = '';
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>() ;
   @Output('bpCreated') bluePrintCreated = new EventEmitter<{serverName: string , serverContent: string}>() ;
 
-  onAddServer() {
-    this.serverCreated.emit({serverName: this.newServerName , serverContent : this.newServerContent}) ;
+  onAddServer(nameInput: HTMLInputElement) {
+    // console.log(nameInput.value) ;
+    // console.log("onAddServer") ;
+    this.serverCreated.emit({serverName: nameInput.value , serverContent : this.newServerContent}) ;
   }
 
-  onAddBlueprint() {
-    this.bluePrintCreated.emit({serverName: this.newServerName , serverContent: this.newServerContent}) ;
+  onAddBlueprint(nameInput :HTMLInputElement) {
+    this.bluePrintCreated.emit({serverName: nameInput.value , serverContent: this.newServerContent}) ;
   }
 
   constructor() { }
